0255b57ac5a28925ab78bc96c62e72ce4fae64c2
Andres Tubelleja III <thethirdandres@gmail.com>
2020-03-09 11:34:15 +0800

    Initial setup

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -32,70 +32,88 @@ class Navigation extends React.Component {
 
   render() {
     return (
-  <nav className = "navbar navbar-expand-lg fixed-top navbar-dark bg-black">
-  <Link to = "/" >
+      <div class="d-flex" id="wrapper">
+        <div class="bg-light border-right" id="sidebar-wrapper">
+          <div class="sidebar-heading">Start Bootstrap </div>
+          <div class="list-group list-group-flush">
+            <a href="#" class="list-group-item list-group-item-action bg-light">Dashboard</a>
+            <a href="#" class="list-group-item list-group-item-action bg-light">Shortcuts</a>
+            <a href="#" class="list-group-item list-group-item-action bg-light">Overview</a>
+            <a href="#" class="list-group-item list-group-item-action bg-light">Events</a>
+            <a href="#" class="list-group-item list-group-item-action bg-light">Profile</a>
+            <a href="#" class="list-group-item list-group-item-action bg-light">Status</a>
+          </div>
+        </div>
+      </div>
+    )
+  }
 
-    <a className ="navbar-brand" >
-    <img src="https://coffeebreak.ph/beta/img/coffeebreakLogo.png" width="150" height="50" className = "d-inline-block align-top" alt="" />
+  // render() {
+  //   return (
+  //   <nav className = "navbar navbar-expand-lg fixed-top navbar-dark bg-black">
+  //   <Link to = "/" >
 
-    </a>
+  //     <a className ="navbar-brand" >
+  //     <img src="https://coffeebreak.ph/beta/img/coffeebreakLogo.png" width="150" height="50" className = "d-inline-block align-top" alt="" />
 
-  </Link>
-    <button ref={input => this.mobileMenuToggler = input} onClick = {this.toggleClassMenu} className = "navbar-toggler" type = "button" data-toggle = "collapse" data-target = "#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className = "navbar-toggler-icon"></span>
-    </button>
+  //     </a>
 
-    <div className = "collapse navbar-collapse" id = "navbarNavDropdown">
-      <ul className = "navbar-nav">
-        <li className = "nav-item ml-md-3">
-            <Link to = "/" className = "nav-link" onClick = {this.closeMenuToggler}>
-                <a>HOME</a>
-            </Link>
-        </li>
-        <li className = "nav-item ml-md-3">
-          <Link to = "/about-us" className = "nav-link" onClick = {this.closeMenuToggler}>
-              ABOUT US
-          </Link>
-        </li>
-        <li className="nav-item dropdown ml-md-3">
-          <Link to = "/menu" className = "nav-link" onClick = {this.closeMenuToggler}>
-            <a className = "dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              MENU
-            </a>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a className="dropdown-item" href="/">Coffee</a>
-              <a className="dropdown-item" href="/">Chocolate</a>
-              <a className="dropdown-item" href="/">Tea</a>
-              <a className="dropdown-item" href="/">Smoothies</a>
-              <a className="dropdown-item" href="/">Matcha</a>
-            </div>
-          </Link>
-        </li>
-        <li className = "nav-item ml-md-3">
-          <Link to = "/store-locator" className = "nav-link" onClick = {this.closeMenuToggler}>
-              STORE LOCATOR
-          </Link>
-        </li>
-        <li className = "nav-item ml-md-3">
-          <Link to = "/partners" className = "nav-link" onClick = {this.closeMenuToggler}>
-              PARTNERS
-          </Link>
-        </li>
-        <li className = "nav-item ml-md-3" onClick = {this.closeMenuToggler}>
-          <Link to = "/careers" className = "nav-link">
-              CAREERS
-          </Link>
-        </li>
-        <li className = "nav-item ml-md-3">
-          <Link to = "/contact" className = "nav-link" onClick = {this.closeMenuToggler}>
-              CONTACT
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </nav>
-)
-}
+  //   </Link>
+  //     <button ref={input => this.mobileMenuToggler = input} onClick = {this.toggleClassMenu} className = "navbar-toggler" type = "button" data-toggle = "collapse" data-target = "#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+  //       <span className = "navbar-toggler-icon"></span>
+  //     </button>
+
+  //     <div className = "collapse navbar-collapse" id = "navbarNavDropdown">
+  //       <ul className = "navbar-nav">
+  //         <li className = "nav-item ml-md-3">
+  //             <Link to = "/" className = "nav-link" onClick = {this.closeMenuToggler}>
+  //                 <a>HOME</a>
+  //             </Link>
+  //         </li>
+  //         <li className = "nav-item ml-md-3">
+  //           <Link to = "/about-us" className = "nav-link" onClick = {this.closeMenuToggler}>
+  //               ABOUT US
+  //           </Link>
+  //         </li>
+  //         <li className="nav-item dropdown ml-md-3">
+  //           <Link to = "/menu" className = "nav-link" onClick = {this.closeMenuToggler}>
+  //             <a className = "dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+  //               MENU
+  //             </a>
+  //             <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+  //               <a className="dropdown-item" href="/">Coffee</a>
+  //               <a className="dropdown-item" href="/">Chocolate</a>
+  //               <a className="dropdown-item" href="/">Tea</a>
+  //               <a className="dropdown-item" href="/">Smoothies</a>
+  //               <a className="dropdown-item" href="/">Matcha</a>
+  //             </div>
+  //           </Link>
+  //         </li>
+  //         <li className = "nav-item ml-md-3">
+  //           <Link to = "/store-locator" className = "nav-link" onClick = {this.closeMenuToggler}>
+  //               STORE LOCATOR
+  //           </Link>
+  //         </li>
+  //         <li className = "nav-item ml-md-3">
+  //           <Link to = "/partners" className = "nav-link" onClick = {this.closeMenuToggler}>
+  //               PARTNERS
+  //           </Link>
+  //         </li>
+  //         <li className = "nav-item ml-md-3" onClick = {this.closeMenuToggler}>
+  //           <Link to = "/careers" className = "nav-link">
+  //               CAREERS
+  //           </Link>
+  //         </li>
+  //         <li className = "nav-item ml-md-3">
+  //           <Link to = "/contact" className = "nav-link" onClick = {this.closeMenuToggler}>
+  //               CONTACT
+  //           </Link>
+  //         </li>
+  //       </ul>
+  //     </div>
+  //   </nav>
+  // )
+  // }
 }
 
 export default Navigation;
