dd81ac3e0ce5322499f301f98f6c065ed6af3296
Mae Dagum <mayeghen@gmail.com>
2020-03-17 15:56:33 +0800

    Fixed home -- setup events component

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -12,11 +12,14 @@ class Events extends React.Component {
               <p id="title-header-left" className="">Special Events</p>
               <p id="title-header-right" className="">WE DO OUT OF THE STORE <br/> EXPERIENCE TOO!</p>
             </div>
-            <div className="bg-red d-flex events-box text-center justify-content-center">
-              <div className="d-flex bg-blue">
+            <div className="d-flex events-box flex-column align-items-center">
+              <div className="d-flex align-items-center h-25">
                 <div className="gold-rec"></div>
-                <div className="events-name overline-16">barista latte art competition</div>
+                <div className="events-name overline-16 pl-3 pr-3">barista latte art competition</div>
                 <div className="gold-rec"></div>
+              </div>
+              <div className="text-light d-flex w-100 h-75 align-items-center justify-content-center">
+                This is a carousel :)
               </div>
             </div>
         </div>
