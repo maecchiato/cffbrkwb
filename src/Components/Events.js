7e03474f9ee3625fd0a84aca27f690cef0c3e9ed
Mae Dagum <mayeghen@gmail.com>
2020-03-18 16:44:19 +0800

    Done Initial Events page
    Pics nlang kulang

--- src/Components/Events.js
+++ src/Components/Events.js
@@ -24,45 +24,107 @@ class Events extends React.Component {
         slidesToShow: 5,
         slidesToScroll: 2
       }
-        return (
+      return (
         <div id = "events-section">
-            <div className="title-header">
-              <p id="title-header-left" className="">Special Events</p>
-              <p id="title-header-right" className="">WE DO OUT OF THE STORE <br/> EXPERIENCE TOO!</p>
+          <div className="title-header">
+            <p id="title-header-left" className="">Special Events</p>
+            <p id="title-header-right" className="">WE DO OUT OF THE STORE <br/> EXPERIENCE TOO!</p>
+          </div>
+          <div className="d-flex events-box flex-column align-items-center">
+            <div className="d-flex align-items-center pt-4 ">
+              <div className="gold-rec"></div>
+              <div className="events-name overline-16 pl-3 pr-3">barista latte art competition</div>
+              <div className="gold-rec"></div>
             </div>
-            <div className="d-flex events-box flex-column align-items-center">
-              <div className="d-flex align-items-center pt-4 ">
-                <div className="gold-rec"></div>
-                <div className="events-name overline-16 pl-3 pr-3">barista latte art competition</div>
-                <div className="gold-rec"></div>
-              </div>
-              <div className="d-flex h-75 justify-content-center w-100" >
+            <div className="d-flex h-75 justify-content-center w-100" >
               <div className="d-flex pt-4 w-75">
                 <Slider {...settings}>
-                    <div>
-                        <img src={img1}/>
-                    </div>
-                    <div>
-                        <img src={img2}/>
-                    </div>
-                    <div>
-                        <img src={img2}/>
-                    </div>
-                    <div>
-                        <img src={img1}/>
-                    </div>
-                    <div>
-                        <img src={img2}/>
-                    </div>
-                    <div>
-                        <img src={img2}/>
-                    </div>
+                  <div>
+                    <img src={img1}/>
+                  </div>
+                  <div>
+                    <img src={img2}/>
+                  </div>
+                  <div>
+                    <img src={img2}/>
+                  </div>
+                  <div>
+                    <img src={img1}/>
+                  </div>
+                  <div>
+                    <img src={img2}/>
+                  </div>
+                  <div>
+                    <img src={img2}/>
+                  </div>
                 </Slider>
-            </div>      
+              </div>      
+            </div>
+          </div>
+          <div className="d-flex events-box flex-column align-items-center">
+            <div className="d-flex align-items-center pt-4 ">
+              <div className="gold-rec"></div>
+              <div className="events-name overline-16 pl-3 pr-3">Tree Planting</div>
+              <div className="gold-rec"></div>
+            </div>
+            <div className="d-flex h-75 justify-content-center w-100" >
+              <div className="d-flex pt-4 w-75">
+                <Slider {...settings}>
+                  <div>
+                    <img src={img1}/>
+                  </div>
+                  <div>
+                    <img src={img2}/>
+                  </div>
+                  <div>
+                    <img src={img2}/>
+                  </div>
+                  <div>
+                    <img src={img1}/>
+                  </div>
+                  <div>
+                    <img src={img2}/>
+                  </div>
+                  <div>
+                    <img src={img2}/>
+                  </div>
+                </Slider>
+              </div>      
+            </div>
+          </div>
+          <div className="d-flex events-box flex-column align-items-center">
+            <div className="d-flex align-items-center pt-4 ">
+              <div className="gold-rec"></div>
+              <div className="events-name overline-16 pl-3 pr-3">Art Cup Competition</div>
+              <div className="gold-rec"></div>
+            </div>
+            <div className="d-flex h-75 justify-content-center w-100" >
+              <div className="d-flex pt-4 w-75">
+                <Slider {...settings}>
+                  <div>
+                    <img src={img1}/>
+                  </div>
+                  <div>
+                    <img src={img2}/>
+                  </div>
+                  <div>
+                    <img src={img2}/>
+                  </div>
+                  <div>
+                    <img src={img1}/>
+                  </div>
+                  <div>
+                    <img src={img2}/>
+                  </div>
+                  <div>
+                    <img src={img2}/>
+                  </div>
+                </Slider>
+              </div>      
+            </div>
           </div>
         </div>
-      </div>
-    )
-}
-}
+      )
+    }
+  }
 export default Events;
