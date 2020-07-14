2e7eb98235252672ae2f7121f2ca84e33d1f2906
Andres Tubelleja III <thethirdandres@gmail.com>
2020-07-14 14:28:16 +0800

    DONE - Partners Page

--- src/Components/Careers.js
+++ src/Components/Careers.js
@@ -50,9 +50,9 @@ class Careers extends React.Component{
     
     return(
     <div id = "careers-section">
-      <div className="title-header">
-        <p id="title-header-left" className="">Be One of Us</p>
-        <p id="title-header-right" className="">FIND YOUR PASSION <br/> AT COFFEEBREAK</p>
+      <div className="title-header d-flex flex-row justify-content-center align-items-center">
+        <div id="title-header-left" className="mr-md-5 mr-3">Be One of Us</div>
+        <div id="title-header-right" className="">FIND YOUR PASSION <br/> AT COFFEEBREAK</div>
       </div>
       <div className="careers-upper-section row">
         <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
@@ -97,25 +97,26 @@ class Careers extends React.Component{
         <div className="h4-barlow text-center">Qualifications</div>
         <div className="d-flex justify-content-center pt-5">
           <div className=" careers-q careers-q-mt pb-5 mb-4 mr-3">
-          <a href="https://www.facebook.com/163120115107442" target="_blank" rel=" noreferrer noopener"><div className="large-card-title subheadline-24 w-75 pt-5 pl-5">Management Trainee</div></a>
-            <div className="large-card-caption body-12-caption-barlow pt-3 pl-5">
-              -- Candidate must possess a Bachelor's Degree in Hotel and Restaurant Management, Hospitality Management, Business Administration/Management or equivalent <br/>
-              -- Confident, honest, trustworthy and dependable <br/>
-              -- Has a strong sense of responsibility and leadership <br/>
-              -- People oriented and has excellent interpersonal skills <br/>
-              -- Possesses good communicating skills both in written and oral <br/>
-              -- Experience in managing store operations is and advantage <br/>
-              -- Fresh graduates are welcome to apply</div>
+            <a href="https://www.facebook.com/163120115107442" target="_blank" rel=" noreferrer noopener"><div className="large-card-title subheadline w-75 pt-5 pl-5">Management Trainee</div></a>
+              <div className="large-card-caption body-12-caption-barlow pt-3 pl-5">
+                <li> Candidate must possess a Bachelor's Degree in Hotel and Restaurant Management, Hospitality Management, Business Administration/Management or equivalent <br/> </li>
+                <li> Confident, honest, trustworthy and dependable </li>
+                <li> Has a strong sense of responsibility and leadership </li>
+                <li> People oriented and has excellent interpersonal skills </li>
+                <li> Possesses good communicating skills both in written and oral </li>
+                <li> Experience in managing store operations is and advantage </li>
+                <li> Fresh graduates are welcome to apply</li>
+              </div>
           </div>
           <div className="careers-q careers-q-dp pb-5 mb-4 ml-3">
-            <a href="https://www.facebook.com/152313069521480" target="_blank" rel=" noreferrer noopener"><div className="large-card-title subheadline-24 w-75 pt-5 pl-5">Barista / Dining Personnel</div></a>
+            <a href="https://www.facebook.com/152313069521480" target="_blank" rel=" noreferrer noopener"><div className="large-card-title subheadline w-75 pt-5 pl-5">Barista / Dining Personnel</div></a>
             <div className="large-card-caption body-12-caption-barlow pt-3 pl-5">
-            -- Candidate should be at least college level or HRM or any food service related course
-            Honest, trustworthy, responsible and dependable <br/>
-            -- Possesses good interpersonal and communication skills <br/>
-            -- Has a sense of urgency <br/>
-            -- Experience in food service is a plus <br/>
-            -- Flexible and can work long hours <br/>
+            <li> Candidate should be at least college level or HRM or any food service related course</li>
+            <li> Honest, trustworthy, responsible and dependable </li>
+            <li> Possesses good interpersonal and communication skills </li>
+            <li> Has a sense of urgency </li>
+            <li> Experience in food service is a plus </li>
+            <li> Flexible and can work long hours </li>
             </div>
           </div>      
         </div>
@@ -129,84 +130,5 @@ class Careers extends React.Component{
 }
 
 
-// ReactDOM.render( <Slider  /> ,
-//   document.getElementById('root')
-// );
-// const Careers = () => (
-//   <div id = "careers-section">
-//     <div className="title-header">
-//       <p id="title-header-left" className="">Be One of Us</p>
-//       <p id="title-header-right" className="">FIND YOUR PASSION <br/> AT COFFEEBREAK</p>
-//     </div>
-//     <div className="careers-upper-section row">
-//       <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-//         <div className="h4-barlow">Fuel Your</div>
-//         <div className="h4-underland h4-subtext">Coffee Passion</div>
-//         <div className="body-16-main-barlow mt-5">
-//         Join us at Coffeebreak. Be part of our groundbreaking company for the years to come!
-//         If you have the heart to own and be one of the Coffeebreak brand, you're the one we are looking for!
-//         </div>
-//       </div>
-//       <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 bg-blue">
-// b
-//       </div>
-
-//     </div>
-//     <div className="careers-lower-section">
-// xczczxczxc
-//     </div>
-//   </div>
-// )
 
 export default Careers;
-
-    {/* <div className = "row mx-0">
-    </div>
-    <div className = "row mt-4 mx-auto pb-5">
-      <div className = "col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-
-      </div>
-      <div className = "col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-        <div className="info-card d-block mx-5">
-        <div className="front">
-          <img src={cbMT} className = "card-image"/>
-          <p className="mt-1 h6 text-center">MANAGEMENT TRAINEE</p>
-            </div>
-          <div className="back">
-            <h6>MANAGEMENT TRAINEE</h6>
-            <ul className = "text-left">
-            <li>Candidate must possess a Bachelo&apos;s Degree in Hotel and Restaurant Management,
-            Hospitality Management, Business Administration/Management or equivalent</li>
-            <li>Confident, honest, trustworthy and dependable</li>
-            <li>Has a strong sense of responsibility and leadership</li>
-            <li>People oriented and has excellent interpersonal skills</li>
-            <li>Possesses good communicating skills both in written and oral</li>
-            <li>Experience in managing store operations is and advantage</li>
-            <li>Fresh graduates are welcome to apply</li>
-            </ul>
-            </div>
-          </div>
-      </div>
-      <div className = "col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-         <div className="info-card d-block mx-5">
-         <div className="front">
-            <img src={cbDP} className = "card-image"/>
-            <p className="mt-1 h6 text-center">BARISTA/DINING PERSONNEL</p>
-         </div>
-         <div className="back">
-           <h6>BARISTA/DINING PERSONNEL</h6>
-           <ul className = "text-left">
-             <li>Candidate should be at least college level or HRM or any food service related course</li>
-             <li>Honest, trustworthy, responsible and dependable</li>
-             <li>Possesses good interpersonal and communication skills</li>
-             <li>Has a sense of urgency</li>
-             <li>Experience in food service is a plus</li>
-             <li>Flexible and can work long hours</li>
-           </ul>
-          </div>
-        </div>
-      </div>
-      <div className = "col-xl-3 col-lg-6 col-md-6 col-sm-12 col-12">
-
-      </div>
-    </div> */}
--- src/Components/Partners.js
+++ src/Components/Partners.js
@@ -15,328 +15,64 @@ class Partners extends React.Component {
   }
 
   render(){
-  const videoOptions = {
-    playerVars:
-    {
-      autoplay: 1,
-      controls: 0,
-      rel: 0,
-      showInfo: 0
-    }
-  };
-  return(
-    <div id="franchisenow" className="partners-section">
-      {/* <div className="bg-img-container-full">
-        <img className="home-bg-img-coffeebean" src={require("../Library/img/home-bg-coffeebean.png")}/>   
-      </div> */}
-      <div className="page-container">
-        <div className="title-header d-flex flex-row justify-content-center align-items-center">
-          <div id="title-header-left" className="mr-md-5 mr-3">Franchise Coffee</div>
-          <div id="title-header-right" className="">GRAB AN EXTRA <br/> BREW OF OPPORTUNITY</div>
-        </div>
-        <div className="partners-upper-section">
-          <div className="subheadline w-50" id="partners-title">You have already experienced the goodness of Coffeebreak, how about getting a franchise?</div>
-          <div className="partners-upper-box">
-            <div className="body-16-main-barlow" id="partners-body"><p>Coffeebreak has been a member of the Philippine Franchise Association since 2012. It aims to be among the top players in the franchise industry and to reach the market in other cities outside Iloilo. </p>
-              Be our partner and continue to promote the taste of local pride. As a Coffeebreak franchisee, you want your investment to be hot and brewed to perfection. And that’s exactly what you will get when you start your business with Coffeebreak.
+    const videoOptions = {
+      playerVars:
+      {
+        autoplay: 1,
+        controls: 0,
+        rel: 0,
+        showInfo: 0
+      }
+    };
+    return(
+      <div id="franchisenow" className="partners-section">
+        <div className="page-container">
+          <div className="title-header d-flex flex-row justify-content-center align-items-center">
+            <div id="title-header-left" className="mr-md-5 mr-3">Franchise Coffee</div>
+            <div id="title-header-right" className="">GRAB AN EXTRA <br/> BREW OF OPPORTUNITY</div>
+          </div>
+          <div className="partners-upper-section">
+            <div className="subheadline" id="partners-title">You have already experienced the goodness of Coffeebreak, how about getting a franchise?</div>
+            <div className="partners-upper-box">
+              <div className="body-16-main-barlow" id="partners-body"><p>Coffeebreak has been a member of the Philippine Franchise Association since 2012. It aims to be among the top players in the franchise industry and to reach the market in other cities outside Iloilo. </p>
+                Be our partner and continue to promote the taste of local pride. As a Coffeebreak franchisee, you want your investment to be hot and brewed to perfection. And that’s exactly what you will get when you start your business with Coffeebreak.
+              </div>
             </div>
           </div>
-        </div>
-        <div className="partners-lower-section">
-          <div className="lower-section-left col-xl-6 col-lg-5 col-md-7 col-xs-12 col-sm-12">
-            {/* REMOVE D-NONEEEEEEE !!!!!!!!!!!!! */}
-            <div className="video-background d-none d-xl-block">
-              <YouTube
-              videoId="GiKGJrc2VbA"
-              opts={videoOptions}
-              className="video-iframe"
-              onReady={this._onReady}
-              onEnd={this._onEnd}
-              />
+          <div className="partners-lower-section">
+            <div className="lower-section-left col-6 d-none d-md-block">
+              {/* REMOVE D-NONEEEEEEE if decided to not show video in mobile view!!!!!!!!!!!!! */}
+              <div className="video-background d-none d-xl-block">
+                <YouTube
+                videoId="GiKGJrc2VbA"
+                opts={videoOptions}
+                className="video-iframe"
+                onReady={this._onReady}
+                onEnd={this._onEnd}
+                />
+              </div>
             </div>
-          </div>
-          <div className="lower-section-right col-xl-6 col-lg-7 col-md-5 col-xs-12 col-sm-12">
-            <div className="mb-5">
-              <div className="h4-barlow">Coffeebreak</div>
-              <div className="h4-underland h4-subtext">Franchise Details</div>
-            </div>
-            <div className="body-16-main-barlow w-75">
-              Package includes the following: <br/>
-              I. Use of the Coffeebreak Trade Name &amp; Marks <br/>
-              II. Business Operation System <br/>
-              III. Store Design, Perspective, and Layout <br/>
-              IV. Complete list of equipment, furniture, required supplies, and Small Wares <br/>
-              V. Marketing Support <br/>
-              VI. Point of Sales System <br/>
-              VII. Operational and Technical Support Training <br/>
-              VIII. Pre-Opening and Start-up
+            <div className="lower-section-right col-12 col-md-6">
+              <div className="mb-5">
+                <div className="h4-barlow">Coffeebreak</div>
+                <div className="h4-underland h4-subtext">Franchise Details</div>
+              </div>
+              <div className="body-16-main-barlow w-75">
+                Package includes the following: <br/>
+                I. Use of the Coffeebreak Trade Name &amp; Marks <br/>
+                II. Business Operation System <br/>
+                III. Store Design, Perspective, and Layout <br/>
+                IV. Complete list of equipment, furniture, required supplies, and Small Wares <br/>
+                V. Marketing Support <br/>
+                VI. Point of Sales System <br/>
+                VII. Operational and Technical Support Training <br/>
+                VIII. Pre-Opening and Start-up
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  ) 
-}
-//   _onReady(event){
-//     event.target.mute();
-// }
-
-// _onEnd(event){
-//   event.target.playVideo();
-// }
-// render(){
-
-//   const videoOptions = {
-//     playerVars:
-//     {
-//       autoplay: 1,
-//       controls: 0,
-//       rel: 0,
-//       showInfo: 0
-//     }
-//   };
-
-//   return(
-//     <div className = "home-section">
-//       <div className="video-background d-none d-xl-block">
-//             <div className="video-foreground">
-//               <YouTube
-//                 videoId="GiKGJrc2VbA"
-//                 opts={videoOptions}
-//                 className="video-iframe"
-//                 onReady={this._onReady}
-//                 onEnd={this._onEnd}
-//               />
-//             </div>
-//         </div>
-//       </div>
-//     )
-//   }
-
+    ) 
+  }
 }
 export default Partners;
-
-// const Partners = () => (
-//   <div id = "partners-section">
-//     <div className="bg-img-container-full"><img className="home-bg-img-coffeebean" src={require("../Library/img/home-bg-coffeebean.png")}/>   </div>
-//     <div className="page-container">
-//       <div className="title-header">
-//         <p id="title-header-left" className="">Franchise Coffee</p>
-//         <p id="title-header-right" className="">GRAB AN EXTRA <br/> BREW OF OPPORTUNITY</p>
-//       </div>
-//       <div className="partners-upper-section">
-//         <div className="subheadline w-50" id="partners-title">You have already experienced the goodness of Coffeebreak, how about getting a franchise?</div>
-//         <div className="partners-upper-box">
-//           <div className="body-16-main-barlow" id="partners-body"><p>Coffeebreak has been a member of the Philippine Franchise Association since 2012. It aims to be among the top players in the franchise industry and to reach the market in other cities outside Iloilo. </p>
-//           Be our partner and continue to promote the taste of local pride. As a Coffeebreak franchisee, you want your investment to be hot and brewed to perfection. And that’s exactly what you will get when you start your business with Coffeebreak.</div>
-//         </div>
-//       </div>
-//       {/* <div className="partners-lower-section">
-//         <div className="lower-section-left">
-
-//         </div>
-//         <div className="lower-section-right">
-
-//         </div>
-//       </div> */}
-//         <div className="partners-lower-section">
-//           <div className="lower-section-left col-xl-5 col-lg-5 col-md-7 col-xs-12 col-sm-12 pt-5">
-//             <p className="subheadline">WHAT'S NEXT FOR WAFFLE TIME?</p>
-//             <p className="body-reg-16">While the company has been a main supporter of various community-based academic and environment-related activities, including the empowerment of local producers through trade exhibits, the recently created Waffle Time Foundation is committed to be a front liner or stimulus in the continuing upliftment of the status of education, the environment and social enterprise in the local communities.</p>
-//             <p className="body-reg-16 font-weight-bold">
-//               Cheers to more years of baking freshness!
-//             </p>
-//             {/* <button className="btn-small">DISCOVER MORE</button> */}
-//           </div>
-//           <div id="foundation-video" className="lower-section-right col-xl-7 col-lg-7 col-md-5 col-xs-12 col-sm-12">
-//             <div className="mt-5 youtube-parent h-100">
-//               <div className="youtube-display pt-5 h-50 d-xl-block d-none">
-//                 <Youtube
-//                   video="mogEZnukIgM"
-//                   autoplay
-//                   muted
-//                   width="80%"
-//                   height="350px"
-//                   showInfo={false}
-//                   controls={false}
-//                   allowFullscreen={false}
-//                   showRelatedVideos={false}
-//                   annotations={false}
-//                   className="video-iframe"
-//                   onReady={this._onReady}
-//                   onEnd={this._onEnd}
-//                 />
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//     </div>
-    {/* <div className = "partners-background row h-100 mx-0">
-      <div className = "partners-box">
-          <h1 className = "partners-title p-4 display-1">GRAB AN EXTRA BREW OF OPPORTUNITY!</h1>
-          <a href = "#info" className = "btn btn-warning">Be our Partner to Success</a>
-      </div>
-    </div>
-    <span id = "info"></span>
-    <div className = "row mx-0 w-100 partners-info">
-      <div className = "col-xl-8 col-lg-8 col-md-6 col-sm-12 col-12">
-        <div className = "info-main ml-5">
-          <p className = "display-4 info-title">
-            You have already experienced the goodness of Coffeebreak, how about getting a franchise?
-          </p>
-          <div className = "mr-5">
-            <p className = "">
-              Coffeebreak has been a member of the Philippine Franchise Association since 2012. It aims to be among the top players
-              in the franchise industry and to reach the market in other cities outside Iloilo.
-            </p>
-            <p className = "">
-              Be our partner and continue to promote the taste of local pride. As a Coffeebreak franchisee, you want your investment to be hot and brewed to perfection.
-              And that’s exactly what you will get when you start your business with Coffeebreak.
-            </p>
-          </div>
-          <div className = "btn-group mt-5">
-            <a href="mailto:franchising@coffeebreak.com?Subject=Franchise%20Inquiry" className = "btn btn-dark">Send us an Email</a>
-            <a href = "#details" className = "btn btn-secondary ml-2">More Details</a>
-          </div>
-          </div>
-        </div>
-          <div className = "col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-            <img src = {awardsimg} className = "d-md-block d-none mx-auto img-fluid" alt = "A Multi Awarded Coffee Shop hailed as the 'Best Coffeeshop in Iloilo' with a whooping constant 5 star rating"/>
-      </div>
-    </div>
-    <span id = "details"></span>
-    <div className = "row mx-0 w-100 partner-details">
-    <div className = "col-12 text-center pb-5">
-      <p className = "display-4 font-weight-bold">Coffeebreak Franchise Details</p>
-    </div>
-      <div className = "col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-        <div className = "partners-faq mx-3">
-          <div className="tab-pane fade active show" id="v-pills-faq" role="tabpanel" aria-labelledby="v-pills-profile-tab"><a href="#" data-toggle="collapse" data-target="#FAQ1">
-              <div className="alert alert-danger" role="alert">
-                Q: What is our Trade Name?</div>
-                </a>
-                <div id="FAQ1" className="collapse">
-                  <p className="p">
-                    A: Coffeebreak Café International, Inc.
-                  </p>
-                </div>
-              </div>
-              <div className="tab-pane fade active show" id="v-pills-faq" role="tabpanel" aria-labelledby="v-pills-profile-tab"><a href="#" data-toggle="collapse" data-target="#FAQ2">
-                  <div className="alert alert-danger" role="alert">
-                    Q: What is the nature of our Business?</div>
-                    </a>
-                    <div id="FAQ2" className="collapse">
-                      <p className="p">
-                        A: Serving food and beverage (Coffee Shop)
-                      </p>
-                    </div>
-                  </div>
-              <div className="tab-pane fade active show" id="v-pills-faq" role="tabpanel" aria-labelledby="v-pills-profile-tab"><a href="#" data-toggle="collapse" data-target="#FAQ3">
-                  <div className="alert alert-danger" role="alert">
-                    Q: How much is our Franchising Fee?</div>
-                    </a>
-                    <div id="FAQ3" className="collapse">
-                      <p className="p">
-                        A: P650, 000.00 Net of Tax
-                      </p>
-                    </div>
-                </div>
-                <div className="tab-pane fade active show" id="v-pills-faq" role="tabpanel" aria-labelledby="v-pills-profile-tab"><a href="#" data-toggle="collapse" data-target="#FAQ4">
-                    <div className="alert alert-danger" role="alert">
-                      Q: Where do we get the construction and renovation, machines and equimpents, furniture and fixture exepnse?</div>
-                      </a>
-                      <div id="FAQ4" className="collapse">
-                        <p className="p">
-                          A: From the Franchisee&apos;s Account
-                        </p>
-                      </div>
-                  </div>
-                  <div className="tab-pane fade active show" id="v-pills-faq" role="tabpanel" aria-labelledby="v-pills-profile-tab"><a href="#" data-toggle="collapse" data-target="#FAQ5">
-                      <div className="alert alert-danger" role="alert">
-                        Q: How Long is our Franchise Duration?</div>
-                        </a>
-                        <div id="FAQ5" className="collapse">
-                          <p className="p">
-                            A: Five (5) years
-                          </p>
-                        </div>
-                    </div>
-                    <div className="tab-pane fade active show" id="v-pills-faq" role="tabpanel" aria-labelledby="v-pills-profile-tab"><a href="#" data-toggle="collapse" data-target="#FAQ6">
-                        <div className="alert alert-danger" role="alert">
-                          Q: How much is the renewal fee?</div>
-                          </a>
-                          <div id="FAQ6" className="collapse">
-                            <p className="p">
-                              A: P500, 000.00
-                            </p>
-                          </div>
-                      </div>
-                      <div className="tab-pane fade active show" id="v-pills-faq" role="tabpanel" aria-labelledby="v-pills-profile-tab"><a href="#" data-toggle="collapse" data-target="#FAQ7">
-                          <div className="alert alert-danger" role="alert">
-                            Q: How much is the Security Deposit?</div>
-                            </a>
-                            <div id="FAQ7" className="collapse">
-                              <p className="p">
-                                A: P100, 000.00 – refundable upon expiration
-                              </p>
-                            </div>
-                        </div>
-                        <div className="tab-pane fade active show" id="v-pills-faq" role="tabpanel" aria-labelledby="v-pills-profile-tab"><a href="#" data-toggle="collapse" data-target="#FAQ8">
-                            <div className="alert alert-danger" role="alert">
-                              Q: Who will select your Store Location?</div>
-                              </a>
-                              <div id="FAQ8" className="collapse">
-                                <p className="p">
-                                  A: You as the franchisee will be the one to choose your strategic location.
-                                  Minimum space required is 60 square meters.
-                                </p>
-                              </div>
-                          </div>
-                      </div>
-            </div>
-          <div className = "col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-          <div className="tab-pane fade active show mx-3" id="v-pills-faq" role="tabpanel" aria-labelledby="v-pills-profile-tab"><a href="#" data-toggle="collapse" data-target="#FAQPackageInclusion">
-              <div className="alert alert-danger" role="alert">
-                Package Includes the foilowing:
-                <i className="fas fa-sort-down float-right"></i>
-              </div>
-                </a>
-                <div id="FAQPackageInclusion" className="collapse">
-                  <ol type="I" className = "h6">
-                    <li>
-                      Use of the Coffeebreak Trade Name & Marks
-                    </li>
-                    <li>
-                      Business Operation System
-                    </li>
-                    <li>
-                      Store Design, Perspective, and Layout
-                    </li>
-                    <li>
-                      Complete list of equipment, furniture, required supplies, and Small Wares
-                    </li>
-                    <li>
-                      Marketing Support
-                    </li>
-                    <li>
-                      Point of Sales System
-                    </li>
-                    <li>
-                      Operational and Technical Support
-                    </li>
-                    <li>
-                      Training
-                    </li>
-                    <li>
-                      Pre-Opening and Start-up
-                    </li>
-                  </ol>
-                </div>
-            </div>
-      </div>
-      <div className = "col-12">
-        <p className = "h6 font-weight-bold text-center pt-3 pb-3">All Rights Reserved 2018 &copy;	</p>
-      </div>
-    </div> */}
-//   </div>
-// )
